2250d3aab0c458f62aa228c546317452223c50a1
Janice <janiceyezhu@gmail.com>
2023-10-30 23:02:55 -0400

    added render routes from homepage

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import { 
+    BrowserRouter as Router, 
+    Routes, 
+    Route, 
+    Link, 
+    Redirect, 
+} from "react-router-dom";
 
 export default class HomePage extends Component {
     constructor (props) {
         super(props);
     }
 
-    render () {
-        return <p>This is home page</p>
+    render() {
+        return ( 
+            <Router>
+                <Routes>
+                    <Route path="/" element={<p>This is the home page</p>} />
+                    <Route path="/join" element={<RoomJoinPage/>} />
+                    <Route path="/create" element={<CreateRoomPage/>} />
+                </Routes>
+            </Router>
+        );
     }
 }
