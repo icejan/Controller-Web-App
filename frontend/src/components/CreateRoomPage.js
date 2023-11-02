217e1e02731b89c138b4039626075d73448f9274
Janice <janiceyezhu@gmail.com>
2023-11-01 20:12:56 -0400

    added methods to control react states
    state is output to browser console

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -15,8 +15,32 @@ export default class CreateRoomPage extends Component {
 
     constructor (props) {
         super(props);
+        this.state = {
+            guestCanPause: true,
+            votesToSkip: this.defaultVotes,
+        };
+
+        this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
+        this.handleVotesChange = this.handleVotesChange.bind(this);
+        this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
     }
 
+    handleVotesChange(e) {
+        this.setState({
+            votesToSkip: e.target.value,
+        });
+    }
+
+    handleGuestCanPauseChange(e) {
+        this.setState ({
+            guestCanPause: e.target.value === "true" ? true : false,
+        });
+    }
+
+    handleRoomButtonPressed() {
+        console.log(this.state);
+    }
+    
     render () {
         return (  
             <Grid container spacing={1}>
@@ -30,7 +54,11 @@ export default class CreateRoomPage extends Component {
                         <FormHelperText>
                             <div align='center'>Guest Control of Playback state</div>
                         </FormHelperText>
-                        <RadioGroup row defaultValue="true">
+                        <RadioGroup 
+                            row 
+                            defaultValue="true" 
+                            onChange={this.handleGuestCanPauseChange}
+                        >
                             <FormControlLabel 
                                 value="true" 
                                 control={<Radio color="primary" />}
@@ -51,6 +79,7 @@ export default class CreateRoomPage extends Component {
                         <TextField 
                             required={true} 
                             type="number" 
+                            onChange={this.handleVotesChange}
                             defaultValue={this.defaultVotes}
                             inputProps={{
                                 min: 1,
@@ -65,7 +94,7 @@ export default class CreateRoomPage extends Component {
                     </FormControl>
                 </Grid>
                 <Grid item xs={12} align="center">
-                    <Button color="primary" variant="contained">
+                    <Button color="primary" variant="contained" onClick={this.handleRoomButtonPressed}>
                         Create A Room
                     </Button>
                 </Grid>
